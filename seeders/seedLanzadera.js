ae667bda52e10d2a6b794675cd71806024d156cf
kbastamow <bastamowkat@gmail.com>
2023-06-07 16:54:02 +0200

    Seed startups in progress, halted

--- seeders/seedLanzadera.js
+++ seeders/seedLanzadera.js
@@ -1,6 +1,7 @@
 const axios = require("axios")
 const Lanzadera = require("../models/Lanzadera")
 const mongoose = require("mongoose");
+const fs = require("fs");
 
 const {dbConnection} = require ("../config/config");
 
@@ -24,18 +25,57 @@ const connectToDatabase = async() => {
     }
 }
 
-  connectToDatabase();
 
- const getStartups = async() => {
-    try {
-        const res = await axios.get() //Url to base de datos de data
-        console.log(res.data)
-        // const startups = await create.Lanzadera()    res.data? What format?
-        // to be continued
+
+//   const processData = async (data) => {
+//     try {
+//       const result = await create.Lanzadera.insertMany(data);
+//       console.log("Documents created:");
+//     } catch (error) {
+//       console.error("Error creating documents:", error);
+//     }
+//   };
+
+// const seedStartups = () => {
+//   console.log("File path:", __dirname + "/path/to/startups_by_rows.json");
+//   fs.readFile("./seeders/startups_by_rows.json", "utf8", (err, data) => {
+//   if (err) {
+//     console.error("Error reading file:", err);
+//     return;
+//   }
+//   try {
+//     const jsonData = JSON.parse(data);
+//     const processedData = jsonData.slice[0,2].map((item) => {  //DECIDE HERE WHAT TO INCLUDe
+//       const { logo_link, ...rest } = item;
+//       return { logo: logo_link, ...rest };
+//     });
+//     console.log(processedData)
+//     // processData(processedData);   //DECIDE HERE WHAT TO INCLUDe
+//   } catch (error) {
+//     console.error("Error parsing JSON data:", error);
+//   }
+// })
+// };
+
+
+
+
+
+//  const getStartups = async() => {
+//     try {
+//         const res = await axios.get() //Url to base de datos de data
+//         console.log(res.data)
+//         // const startups = await create.Lanzadera()    res.data? What format?
+//         // to be continued
           
     
-    } catch (error) {
+//     } catch (error) {
         
-    } 
+//     } 
 
- }
+//  }
+
+
+ connectToDatabase();
+seedStartups()
+
